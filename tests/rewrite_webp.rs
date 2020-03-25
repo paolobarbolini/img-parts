83884b5654fd321d9520fcd04b9936199fbb3996
Paolo Barbolini <paolo@paolo565.org>
2020-03-25 15:13:27 +0100

    tests: add a rewrite webp test

---
+++ tests/rewrite_webp.rs
@@ -0,0 +1,33 @@
+use std::fs;
+
+use icc_editor::webp::WebP;
+
+#[test]
+fn reqwrite_webp_noprofile() {
+    extract_webp_image("P1133897.webp");
+}
+
+#[test]
+fn reqwrite_webp_srgb() {
+    extract_webp_image("P1133897_sRGB.webp");
+}
+
+#[test]
+fn reqwrite_webp_adobergb() {
+    extract_webp_image("P1133897_AdobeRGB.webp");
+}
+
+#[test]
+fn reqwrite_webp_out() {
+    extract_webp_image("P1133897.out.webp");
+}
+
+fn extract_webp_image(input: &str) {
+    let file = fs::read(format!("tests/{}", input)).expect("read webp");
+
+    let webp = WebP::read(&mut &file[..]).unwrap();
+
+    let mut bytes = Vec::new();
+    webp.write_to(&mut bytes).expect("write webp");
+    assert_eq!(file, bytes);
+}
