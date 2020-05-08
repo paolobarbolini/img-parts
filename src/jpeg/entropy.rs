4f010153ef47fe318a6e01eb11691049e20bc29f
Paolo Barbolini <paolo@paolo565.org>
2020-05-08 17:17:09 +0200

    jpeg: simplify Entropy
    We shouldn't care about decoding the entropy since that's not what this crate
    is about. Instead we are now just reading the raw entropy and writing it back
    when saving the file.
    
    This also removes the dependency on bytecount.

--- src/jpeg/entropy.rs
+++ src/jpeg/entropy.rs
@@ -6,57 +6,41 @@ use super::markers;
 
 #[derive(Clone, PartialEq)]
 pub struct Entropy {
-    data: Vec<u8>,
-    markers: Vec<usize>,
+    raw: Vec<u8>,
 }
 
 #[allow(clippy::len_without_is_empty)]
 impl Entropy {
     pub(crate) fn read(r: &mut dyn Read) -> Result<Entropy> {
-        let mut pos = 0;
-        let mut data = Vec::new();
-        let mut markers = Vec::new();
+        let mut raw = Vec::new();
 
         loop {
             let byte = r.read_u8()?;
             if byte != markers::P {
-                pos += 1;
-                data.push(byte);
+                raw.push(byte);
                 continue;
             }
 
             let marker_byte = r.read_u8()?;
             match marker_byte {
                 markers::EOI => {
-                    return Ok(Entropy { data, markers });
-                }
-                markers::Z => {
-                    pos += 1;
-                    data.push(byte)
+                    return Ok(Entropy { raw });
                 }
                 _ => {
-                    markers.push(pos);
-                    data.push(byte);
-                    data.push(marker_byte);
-                    pos += 2;
+                    raw.push(byte);
+                    raw.push(marker_byte);
                 }
             }
         }
     }
 
     pub fn len(&self) -> usize {
-        // data + # byte stuffing bytes + EOI marker (2 bytes)
-        self.data.len() + bytecount::count(&self.data, markers::P) - self.markers.len() + 2
+        // raw data + EOI marker (2 bytes)
+        self.raw.len() + 2
     }
 
     pub(crate) fn write_to(&self, w: &mut dyn Write) -> Result<()> {
-        for (pos, byte) in self.data.iter().enumerate() {
-            w.write_u8(*byte)?;
-
-            if *byte == markers::P && !self.markers.contains(&pos) {
-                w.write_u8(markers::Z)?;
-            }
-        }
+        w.write_all(&self.raw)?;
 
         w.write_u8(markers::P)?;
         w.write_u8(markers::EOI)?;
@@ -73,28 +57,25 @@ mod tests {
         let input: &[u8] = &[
             0xE2, 0xFF, 0xE2, 0x51, 0xE7, 0xFF, 0x00, 0xAA, 0xFD, 0xFF, 0x00, 0xCA, 0xFF, 0xD9,
         ];
-        let expected_data: &[u8] = &[0xE2, 0xFF, 0xE2, 0x51, 0xE7, 0xFF, 0xAA, 0xFD, 0xFF, 0xCA];
-        let expected_markers: &[usize] = &[1];
 
         let output = Entropy::read(&mut &input[..]).expect("read_entropy");
         assert_eq!(output.len(), input.len());
-        assert_eq!(output.data.as_slice(), expected_data);
-        assert_eq!(output.markers.as_slice(), expected_markers);
+        assert_eq!(output.raw.as_slice(), &input[..input.len() - 2]);
     }
 
     #[test]
     fn test_write_entropy() {
-        let data = vec![0xE2, 0xFF, 0xE2, 0x51, 0xE7, 0xFF, 0xAA, 0xFD, 0xFF, 0xCA];
-        let markers = vec![1];
-        let expected: &[u8] = &[
-            0xE2, 0xFF, 0xE2, 0x51, 0xE7, 0xFF, 0x00, 0xAA, 0xFD, 0xFF, 0x00, 0xCA, 0xFF, 0xD9,
-        ];
+        let mut raw = vec![0xE2, 0xFF, 0xE2, 0x51, 0xE7, 0xFF, 0xAA, 0xFD, 0xFF, 0xCA];
 
-        let entropy = Entropy { data, markers };
+        let entropy = Entropy { raw: raw.clone() };
 
         let mut output = Vec::new();
         entropy.write_to(&mut output).expect("write_entropy");
-        assert_eq!(entropy.len(), expected.len());
-        assert_eq!(output.as_slice(), expected);
+
+        raw.push(markers::P);
+        raw.push(markers::EOI);
+
+        assert_eq!(entropy.len(), raw.len());
+        assert_eq!(output.as_slice(), raw.as_slice());
     }
 }
