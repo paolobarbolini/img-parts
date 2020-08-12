3e8ae2dd98b5311b98fb69f71bef1d05a169b773
Paolo Barbolini <paolo@paolo565.org>
2020-08-12 19:17:11 +0200

    webp: fix TODO

--- src/webp/mod.rs
+++ src/webp/mod.rs
@@ -113,8 +113,7 @@ impl WebP {
             let flags = WebPFlags::from_webp(self);
             let mut content = BytesMut::with_capacity(10);
 
-            // TODO: change this mess
-            content.put(Bytes::from(flags.0.to_vec()));
+            content.extend_from_slice(&flags.0.to_vec());
 
             let buf = u24_to_le_bytes(width - 1);
             content.extend_from_slice(&buf);
